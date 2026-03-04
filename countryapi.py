00acc1bc0f80c7693f512fcafc7fe0762d470b63
socraya09-debug <socraya09@gmail.com>
2026-03-04 14:25:40 -0400

    no

--- countryapi.py
+++ countryapi.py
@@ -16,3 +16,11 @@ export const getFlights = (req, res) => {
     }
   ]);
 };
+import express from "express";
+import { getFlights } from "../controllers/flightsController.js";
+
+const router = express.Router();
+
+router.get("/", getFlights);
+
+export default router;
