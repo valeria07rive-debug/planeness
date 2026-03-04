5dedde5e11ed94a6a008615dd1909699832a79c7
socraya09-debug <socraya09@gmail.com>
2026-03-04 14:26:54 -0400

    okok

--- countryapi.py
+++ countryapi.py
@@ -1,13 +1,9 @@
 import express from "express";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Lista de vuelos" });
-});
-
-export default router;
-
-export const getFlights = (req, res) => {
+// Controller
+const getFlights = (req, res) => {
   res.json([
     {
       id: 1,
@@ -16,11 +12,8 @@ export const getFlights = (req, res) => {
     }
   ]);
 };
-import express from "express";
-import { getFlights } from "../controllers/flightsController.js";
 
-const router = express.Router();
-
+// Route
 router.get("/", getFlights);
 
 export default router;
